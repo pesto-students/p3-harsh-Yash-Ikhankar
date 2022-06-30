a6d453b818357cdcbbfb0f082c5a6aaca36b718c
Yash-Ikhankar <96953168+Yash-Ikhankar@users.noreply.github.com>
2022-06-30 23:52:00 +0530

    base added

---
+++ Week-3/Exercise-3-3/indes.js
@@ -0,0 +1,17 @@
+unctioncreateIncrement() {
+  letcount=0;
+  functionincrement() {
+    count++;
+  }
+  letmessage=`Count is${count}`;
+  functionlog() {
+    console.log(message);
+  }
+  
+  return[increment,log];
+}
+const[increment,log] =createIncrement();
+increment();
+increment();
+increment();
+log();// What is logged?
