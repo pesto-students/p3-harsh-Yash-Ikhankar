6ef0243263bca2696d3df5e2dcf9a42583a4e568
Yash-Ikhankar <96953168+Yash-Ikhankar@users.noreply.github.com>
2022-07-23 23:48:07 +0530

    completed

---
+++ Week-6/Exercise 6-4/Besttimetobuyandsellstock.js
@@ -0,0 +1,15 @@
+var maxProfit = function(prices) {
+  let max = 0;
+  let min = Infinity;
+  for(let i = 0; i < prices.length; i++) {
+      min = Math.min(min, prices[i]);
+      max = Math.max(max, prices[i] - min);
+  }    
+  
+  return max;
+};
+
+let prices = [7,1,5,3,6,4];
+
+console.log(maxProfit(prices));
+
