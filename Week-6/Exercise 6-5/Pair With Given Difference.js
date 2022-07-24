6556a9622535b4a365d1008448d47bf3b6fdbac5
Yash-Ikhankar <96953168+Yash-Ikhankar@users.noreply.github.com>
2022-07-24 18:10:38 +0530

    completed

---
+++ Week-6/Exercise 6-5/Pair With Given Difference.js
@@ -0,0 +1,15 @@
+const pairDiff = (nums, result) => {
+  let sortedNums = nums.sort((a, b) => a - b);
+  for (let i = 0; i < sortedNums.length; i++) {
+    let value = sortedNums[i] + result;
+    if (sortedNums.indexOf(value) !== -1) {
+      return 1;
+    }
+  }
+  return 0;
+};
+
+console.log(pairDiff([5, 10, 3, 2, 50, 80], 78));
+console.log(pairDiff([-10, 20], 30))
+/*time complexity - n(n)
+space complexity - 0(n)*/
